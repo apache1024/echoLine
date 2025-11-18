db9db1911bde8046b814c1e7ac9cee2711af49fe
Andrei Abramov <apache25@yandex.ru>
2025-11-18 17:39:48 +0700

    Добавил функцию предусматривающую внешние переменные в значении аргумента outputFile

--- echoLine.sh
+++ echoLine.sh
@@ -3,6 +3,7 @@
 # Укажите имя файла, который вы хотите прочитать
 input_file=$1
 outputFile=$2
+out_var=$3 #если это условие не пустое outputFile становиться переменной создаваемого скрипта, и получает знак $ в начале
 
 # Проверяем, существует ли файл
 if [[ ! -f "$input_file" ]]; then
@@ -15,11 +16,20 @@ if [[  -f "$input_file.~" ]]; then
     rm "$input_file.~"
 fi
 
+if [[ -n "$3" ]]; then
+    outputFile='$'$outputFile # если третий аргумент не пустой то добавляем знак $ к значению $outputFile
 
+fi
 
 
 echo '# ############### РЕДАКТУРА '$outputFile' ################################'> $input_file.~
-echo 'if [[ -f "'$outputFile'" ]]; then'>>$input_file.~
+if [[ -n "$3" ]]; then
+    echo 'if [[ -f '$outputFile' ]]; then'>>$input_file.~ #если $outputFile переменная то проверка без "" ковычек
+else
+    echo 'if [[ -f "'$outputFile'" ]]; then'>>$input_file.~ #если $outputFile прямой путь к файлу то с "" ковычками
+
+fi
+
 
 echo '     mv '$outputFile $outputFile'.`date +%s` # муваем файл чтобы сохранить оригинал'  >> $input_file.~
 echo 'fi' >> $input_file.~
